46f4639016f827c7fddd60a83e58283065352fc4
Anna Park <annapark23@gmail.com>
2018-10-28 18:26:41 -0500

    was able to kind of code random letter generator, and the scoring

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //start the game with a score of 0 
-var wins = 0;
-var loses = 0;
+var win = 0;
+var lose = 0;
 
 //start the game with 10 lives
 var lives = 10; 
@@ -8,33 +8,59 @@ var lives = 10;
 //creates a list that contains the whole alphabet 
 var alphabet=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k","l","m","n","o","p","q","r","s","t","u","v", "w", "x", "y", "z"];
 
-// Create variables that hold references to the places in the HTML where we want to display things.
-var remainingGuesses = document.getElementById("remaining-guesses");
-var scoreWins = document.getElementById("wins");
+//empty array where letters can be stored? 
+var usedLetters =[];
 
+// Create variables that hold references to the places in the HTML where we want to display things.
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var remainingGuessesText = document.getElementById("remaining-guesses-text");
+var lettersGuessedText = document.getElementById("letters-guessed-text");
+
+//FUNCTIONS
+// ----------------------------------------------------------------------------------------- <-- looks cool i guess
+
+//function to store the computer guess in an empty string. 
+function compGuess(){
+    var randomLetter = alphabet[Math.floor(Math.random()*alphabet.length)];  
+    console.log(randomLetter); 
+    var computerGuess = [];
+    computerGuess.push(randomLetter); 
+    
+}
 
 
 
 // function is run whenever a user presses a key...
 document.onkeyup = function(event) {
 
+    compGuess();
+
     // Determines which key was pressed.
     var userGuess = event.key;
+    var userStorage = [];
+    userStorage.push(userGuess); 
 
-    //produce a random letter :) 
-    var randomLetter = alphabet[Math.floor(Math.random()*alphabet.length)]; 
+    if (userGuess === compGuess){
+        wins ++;
+        lives = 10;
+        usedletters = [];
+    }
 
-//    if (userGuess != randomLetter){
-  //      lives -=1;
-   // }
+    if(userGuess !== compGuess){
+        lives -= 1; 
+    }
 
-    //if (userGuess == randomLetter){
-    //    wins++;
-    //}
+    if (lives ===0){
+        lose++;
+        usedLetters =[];
+        lives = 10;
+    }
 
+    // Display the user and computer guesses, and wins/losses/ties.
+    winsText.textContent = "Wins: " + win;
+    lossesText.textContent = "Losses: " + lose;
+    remainingGuessesText.textContent = "Guesses left: " + lives;
+    lettersGuessedText.textContent = "Your guesses so far: " + userGuess;
 
-    
-    remainingGuess = "Your guesses so far: " + userGuess
-    scoreWins = "Wins: " + wins;
-
-}
+};
