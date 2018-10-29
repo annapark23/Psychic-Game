ee611d7aee01ea67fe27e644353b2e9f45837e1c
Anna Park <annapark23@gmail.com>
2018-10-28 21:25:46 -0500

    finished my codeeee

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,12 +6,15 @@ var lose = 0;
 var lives = 10; 
 
 //creates a list that contains the whole alphabet 
-var alphabet=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k","l","m","n","o","p","q","r","s","t","u","v", "w", "x", "y", "z"];
+var alphabet="qwertyuiopasdfghjklzxcvbnm";
 
 //empty array where letters can be stored? 
 var usedLetters =[];
 
+var randomLetter = randomLetter;
+
 // Create variables that hold references to the places in the HTML where we want to display things.
+var guessIntroText = document.getElementById("guess-intro");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var remainingGuessesText = document.getElementById("remaining-guesses-text");
@@ -20,44 +23,59 @@ var lettersGuessedText = document.getElementById("letters-guessed-text");
 //FUNCTIONS
 // ----------------------------------------------------------------------------------------- <-- looks cool i guess
 
-//function to store the computer guess in an empty string. 
-function compGuess(){
-    var randomLetter = alphabet[Math.floor(Math.random()*alphabet.length)];  
-    console.log(randomLetter); 
-    var computerGuess = [];
-    computerGuess.push(randomLetter); 
-    
+
+randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+console.log(randomLetter);
+
+//Uses random whole number generated above to select a random letter from the array [letters] and assigns it to the var ranLetter
+function compGuess() {
+    randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(randomLetter);
+
 }
-
-
 
 // function is run whenever a user presses a key...
 document.onkeyup = function(event) {
 
-    compGuess();
 
     // Determines which key was pressed.
     var userGuess = event.key;
-    var userStorage = [];
-    userStorage.push(userGuess); 
 
-    if (userGuess === compGuess){
-        wins ++;
+    //if you guessed correctly, add a win, lives and letters guessed will be reset, and comp will get another word. 
+    if (userGuess === randomLetter){
+        win ++;
         lives = 10;
-        usedletters = [];
+        usedLetters = [];
+        compGuess();
     }
 
-    if(userGuess !== compGuess){
+    //take away a life if userguess doesnt equal random letter
+    if(userGuess !== randomLetter){
         lives -= 1; 
     }
 
+    //if lives = 0
     if (lives ===0){
         lose++;
         usedLetters =[];
         lives = 10;
+        compGuess();
+    } 
+
+    //do nothing if user guess is in usedletter array 
+    if (usedLetters.indexOf(userGuess) >= 0) {
+
+    } 
+    else {
+        //this pushes the players incorrect guess to the usedArray and writes it to the HTML
+        usedLetters.push(userGuess);
+        document.getElementById('userGuess').innerHTML = usedLetters;
+
     }
 
+
     // Display the user and computer guesses, and wins/losses/ties.
+    guessIntroText.textContent = "Guess what letter I'm thinking of..";
     winsText.textContent = "Wins: " + win;
     lossesText.textContent = "Losses: " + lose;
     remainingGuessesText.textContent = "Guesses left: " + lives;
